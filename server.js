5b48f9824f46a09d445b8e9802e4f351a4998bfd
Furkan Ilısu <frkn.ilisu@gmail.com>
2016-02-20 07:50:00 +0200

    bozuk location

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
+var assert = require('assert');
 //var http = require('http');
-//var sys = require('util'),
 URL = require('url'),
 qs = require('querystring')
 
@@ -9,10 +9,12 @@ var mongoClient = require('mongodb').MongoClient;
 
 var MONGODB_URI = "mongodb://admin:123456@ds011278.mongolab.com:11278/heroku_hs6w48x7";
 
+var storeID;
+
 // This responds with "Hello World" on the homepage
-app.get('/', function (req, res) {
+app.get('/', function (request, response) {
    console.log("Got a GET request for the homepage");
-   res.send('Hello GET');
+   response.send('Hello Getir-Hackathon');
 })
 
 app.get('/login', function(request, response){
@@ -21,9 +23,32 @@ app.get('/login', function(request, response){
 	var user_name = url_params.query.name;
 	var user_password = url_params.query.password;
 	console.log("User name = " + user_name + ", password is " + user_password);
-	response.end("yes");
+	
+	mongoClient.connect(MONGODB_URI, function(err, db) {
+		if (err) throw err;
+		console.log("Connected to Database");
+		checkUser(db, function(message) {
+			response.end(message);
+			db.close();
+		}, user_name, user_password);
+	});
 	
 })
+
+var checkUser = function(db, callback, userName, userPass) {
+	
+	var cursor = db.collection('users').find( );
+	cursor.each(function(err, doc) {
+		assert.equal(err, null);
+		if (doc != null) {
+			if(doc.id == userName && doc.pass == userPass){
+				callback("Login successfully: " + userName);
+			}
+		} else {
+			callback();
+		}
+	});
+};
 
 app.get('/signup', function(request, response){
 	
@@ -31,12 +56,14 @@ app.get('/signup', function(request, response){
 	var user_name = url_params.query.name;
 	var user_password = url_params.query.password;
 	console.log("User name = " + user_name + ", password is " + user_password);
-	response.end("yes");
 	
 	mongoClient.connect(MONGODB_URI, function(err, db) {
 		if (err) throw err;
 		console.log("Connected to Database");
-		insertUser(db, function(){db.close();}, user_name, user_password);
+		insertUser(db, function(message){
+			response.end(message);
+			db.close();
+		}, user_name, user_password);
 	});
 	
 })
@@ -50,68 +77,114 @@ var insertUser = function(db, callback, userName, userPass){
 			console.log("error insert:" + err);
 		} else{
 			console.log("Inserted a document into the restaurants collection.");
-			callback();
+			callback("Inserted a document into the restaurants collection" + userName);
 		}
 	}
 }
 
-
-
-// This responds a GET request for the /list_user page.
-app.get('/database', function (request, response) {
-	
-	mongoClient.connect(MONGODB_URI, function(err, db){
-		
-		if(err){
-			console.log('Unable to connect to the mongoDB server. Error:', err);
-		} else{
-			//HURRAY!! We are connected. :)
-			console.log('Connection established to', MONGODB_URI);
+app.get('/location', function(request, response){
+	var url_params = URL.parse(request.url , true);
+	var x = url_params.query.x;
+	var y = url_params.query.y;
+	var p_id = new Array();
+	mongoClient.connect(MONGODB_URI, function(err, db) {
+		if (err) throw err;
+		console.log("Connected to Database");
+	;
+		findProducts(db , function(message){
 			
-			var url_parts;
-			if(request.method == 'GET'){
-				url_parts = URL.parse(request.url, true);
-				response.writeHead( 200 );
-				response.write( JSON.stringify( url_parts.query ) );
-				response.end();
-				
-				
-			} else if (request.method == 'POST') {
-                var body = '';
-                request.on('data', function (data) {
-                    body += data;
-                });
-                request.on('end',function() {
-                    var POST =  qs.parse(body);
-                    //console.log(POST);
-                    response.writeHead( 200 );
-                    response.write( JSON.stringify( POST ) );
-                    response.end();
-                });
-			}
-
-			// do some work here with the database.			
-								// Get the documents collection
-				var cursor = db.collection('users').find( );
-				cursor.each(function(err, doc) {
-					if(err){
-						console.log("error: " + err);
-					} else{
-						if (doc != null) {
-							console.dir(doc);
-						} else {
-							console.log("no doc");
-						}
+		db.close();} , x, y)
+			
+});
+});
+function findProducts(db , callback , x , y){
+			//Find Closest Store Id
+				var result = new Array()
+		var cursor = db.collection('stores').find();
+		cursor.each(function(err, doc) {
+			if(err){
+				console.log("error: " + err);
+			} else{
+			
+				if (doc != null) {
+					if(getDistanceFromLatLonInKm(doc.loc.x , doc.loc.y , x , y) <= 10){
+						storeID = doc._id;		
+						console.log("Store id: " + storeID);
+					
+					
+						
+						var cursor2 = db.collection('stocks').find( { "store_id": storeID } );
+						cursor2.each(function(err, doc){
+							if(err) throw err;
+							if(doc != null){
+								if(doc.stocks > 0){
+									result.push(doc.product_id);
+									console.log("products : " + doc.product_id);
+								/*	var cursor3 = db.collection('products').find( { "_id": doc.product_id } );
+									cursor3.each(function(err, doc){	
+									if(doc!= null){
+									console.log("products : " + doc.name);
+									var jsonArg = new Object();
+									jsonArg.name = doc.name ;
+									result.push(jsonArg);
+									console.log("vdsbndfn");
+									}
+									else
+										console.log("no doc");
+								);*/
+									callback(result);
+								}
+								
+							} else{
+								console.log("no doc for this storeID:" + storeID);
+							}
+							
+							
+						});
+						
+						
 					}
-				});
+					
+				} else {
+					console.log("no doc for stores");
+				}
+			}
 			
+		});
+}
 
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+	var R = 6371; // Radius of the earth in km
+	var dLat = deg2rad(lat2-lat1);  // deg2rad below
+	var dLon = deg2rad(lon2-lon1); 
+	var a = 
+	Math.sin(dLat/2) * Math.sin(dLat/2) +
+	Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+	Math.sin(dLon/2) * Math.sin(dLon/2)
+	; 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c; // Distance in km
+	return d;
+}
+
+function deg2rad(deg) {
+	return deg * (Math.PI/180)
+}
+
+
+
+function findProduct(db, id){
+		
+	var cursor = db.collection('products').find( { "_id": id } );
+	cursor.each(function(err, doc){
+		if(doc != null){
+			return("{ name : " + doc._id +"  price: " + doc.price +" imgUrl: " + doc.imgUrl + "weight: " + doc.weight+ "}");
 		}
 	});
-})
+}
 
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
 
   var host = server.address().address
   var port = server.address().port
